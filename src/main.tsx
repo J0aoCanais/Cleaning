e6a270e440c1e2d5f2f0af81434894d41b6e07a2
gouveiaaveia <gamerkiko1976@gmail.com>
2025-03-17 16:18:20 +0000

    tudo

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
-    <div className="w-[329px] h-[253px]"></div>
   </React.StrictMode>,
 );
